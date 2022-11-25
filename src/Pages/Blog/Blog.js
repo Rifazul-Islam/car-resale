a702dc38782e46f5db6f3f01afe5bf865d49292b
Rifazul <rifazul50@gmail.com>
2022-11-25 20:01:43 +0600

    add blog page answer complied

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -41,6 +41,18 @@ const Blog = () => {
   </div>
 
 
+  <div className="card card-side bg-indigo-300 mt-5 w-full shadow-xl">
+      <div className="card-body">
+      <div className=' grid justify-items-start'>
+      <h2 className=" text-2xl grid justify-start "> 4 .  React vs. Angular vs. Vue?</h2>
+      <h2 className=" font-bold mt-2 text-xl text-justify "> There are three frameworks for building web applications that every frontend developer has heard about: React, Vue.js, and Angular.React is a UI library, Angular is a fully-fledged front-end framework, while Vue.js is a progressive framework.
+They can be used almost interchangeably to build front-end applications, but they’re not 100 percent the same, so it makes sense to compare them and understand their differences.
+Each framework is component-based and allows the rapid creation of UI features.</h2>
+      </div>
+   </div>
+  </div>
+
+
 </div>
     );
 };
