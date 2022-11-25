1b28d9144eb2dae08b357dca6ca14985d79849f4
Rifazul <rifazul50@gmail.com>
2022-11-25 10:14:44 +0600

    category dainamic page add

--- src/Pages/Home/Categories/CategoriCard.js
+++ src/Pages/Home/Categories/CategoriCard.js
@@ -11,7 +11,7 @@ const CategoriCard = ({product}) => {
     <h2 className="card-title"> {name} </h2>
     <p>If a dog chews shoes whose shoes does he choose?</p>
     <div className="card-actions justify-end">
-      <button className="btn btn-outline w-full"> <Link to={`/category/${_id}`} > {category}</Link> </button>
+    <Link className="btn btn-outline w-full"  to={`/category/${_id}`} ><button > {category} </button> </Link>
     </div>
   </div>
 </div>
--- src/Pages/Home/Category/CategoCard.js
+++ src/Pages/Home/Category/CategoCard.js
@@ -1,21 +1,40 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../contexts/AuthProvider';
 
 const CategoCard = ({secondhand}) => {
+   
+      const {user} = useContext(AuthContext)
     const {img,name,Locatin,resalePrice,originalPrice,years,} = secondhand
+
+
     return (
-        <div className='   '>
-        <div className="card card-compact h-[400px]   w-80 bg-base-200 shadow-xl">
-        <figure><img src={img} alt="Shoes" /></figure>
-     <div className="card-body">
-   <h2 className="card-title"> {name} </h2>
-   <p>If a dog chews shoes whose shoes does he choose?</p>
-   <div className="card-actions justify-end">
-     <button className="btn btn-outline w-full"> click </button>
-   </div>
- </div>
-</div>
+        <div className='mb-5 mt-3'>
+            <div className="card card-compact h-[400px]  flex justify-between  bg-base-200 shadow-xl">
+         <figure><img src={img} alt="Shoes" /></figure>
+      <div className="card-body">
+       <div className='grid justify-items-start '>
+       <p className='ml-7 font-bold'> Location :  {name}</p>
+        <p className='ml-7 font-bold'> Location :  {Locatin} </p>
+       <p className='ml-7 font-bold'>  Resale Price: {resalePrice}Tk </p>
+       <p className='ml-7  font-bold'> Original Price : {originalPrice} </p>
+       <p className='ml-7 font-bold'> Youre : {years} </p>
+       <p className='ml-7 font-bold'> Seller Name : {user?.displayName} </p>
+
+      
        </div>
+      
+     </div>
+     <div className="card-actions justify-end mb-5 mr-3">
+     <button className="btn btn-success btn-sm ">Booking Now</button>
+     </div>
+  </div>
+</div>
+    
     );
 };
 
 export default CategoCard;
+
+
+    
